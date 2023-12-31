340af3f658ed0cababc8cd73d3c05cc484e926c3
Leonardo Lucena <leonardolucena.cc@gmail.com>
2023-12-30 22:56:17 -0300

    feat: Configure the socket

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
+import { Socket } from 'dgram';
 import express from 'express';
 import http from 'http';
+import socketio from 'socket.io';
 
 const app = express();
 const server = http.Server(app);
 
+app.use(express.static(__dirname + '/public'))
+
+const io = socketio(server);
+
+io.on('connect', (socket) => {
+    io.to(socket.id).emit({
+        status: true,
+        message: "Conexão estabelecida"
+    })
+
+    socket.on('teste', (res) => {
+        console.log('Mensagem recebida!', res);
+
+        io.to(socket.id).emit(res);
+    })
+});
+
 app.get('/', (req, res) => {
-    res.send('Hello wolrd');
+    res.render('index.html');
 })
 
 server.listen(60000, () => {
